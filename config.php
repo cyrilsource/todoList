85e8927a48b794d8eac668314e56e6e4e82bf1ac
cyrilsource <cyrilbron78@gmail.com>
2016-05-05 18:20:16 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php 
 
 $config = array(
-	'host'		=> '127.0.0.1',
-	'username'	=> 'root',
-	'password'	=> 'florence',
-	'database'	=> 'todoList',
+	'host'		=> '',
+	'username'	=> '',
+	'password'	=> '',
+	'database'	=> '',
 	'fr'		=> 'fr_FR'
 
 
